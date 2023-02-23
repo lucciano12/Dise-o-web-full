df7a934ec58155ca433ad671b2a2dd4bac598436
Lucciano99 <lpradoe@hotmail.com>
2023-02-23 11:41:13 -0300

    Repaso a Java parte 1 listo
    Listo

---
+++ Introduccion HTML/Pratica JavaScript/IntroduccionJS/js/Funciones.js
@@ -0,0 +1,16 @@
+
+// Declaración de Función
+function sumar() {
+    console.log(10 + 10);
+}
+sumar();
+
+
+
+// Expresión de la función (Esta marca error, sale pregunta en postulacion de pega )
+sumar2();
+const sumar2 = function() {
+    console.log( 3 + 3);
+}
+
+
