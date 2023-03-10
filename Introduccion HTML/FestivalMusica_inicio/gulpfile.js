ba6883498a2cfbe0bdbaa962c22223de8757124f
Lucciano99 <lpradoe@hotmail.com>
2023-03-10 01:06:27 -0300

    10/03

--- Introduccion HTML/FestivalMusica_inicio/gulpfile.js
+++ Introduccion HTML/FestivalMusica_inicio/gulpfile.js
@@ -1,15 +1,64 @@
 /* Contendeor de nuestras tareas, aqui se realizaran las funciones y se conectara 
 al package.json. Entonces gulp en general es para optimizar tareas */
 
-const {src, dest, watch} = require("gulp"); //require importar o extraer la dependencia de gulp
+const {src, dest, watch, parallel} = require("gulp"); //require importar o extraer la dependencia de gulp
 /* Src --> Identificar un archivo
  dest --> almacenar el archivo */
+
+ //CSS
 const sass = require('gulp-sass')(require('sass')); // sass en el const, es la funcion a llamar cuando lo compilemos ya que estamos importando el script del package.json
 const plumber = require('gulp-plumber'); //Importamos plumber
 
 
-function css(done){
 
+// Imagenes 
+const cache = require('gulp-cache');
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp'); 
+const avif = require('gulp-avif');
+
+
+
+function versionWebp(done){
+
+  const opciones = {
+    quality:50
+  };
+
+  src('src/img/**/*.{png,jpg}')
+  .pipe(webp(opciones))
+  .pipe(dest('build/img'))
+
+  done();
+}
+
+function versionAvif(done){
+
+  const opciones = {
+    quality:50
+  };
+
+  src('src/img/**/*.{png,jpg}')
+  .pipe(avif(opciones))
+  .pipe(dest('build/img'))
+
+  done();
+}
+
+function imagenes(done){
+
+  const opciones = {
+    optimizationLevel: 3
+  }
+
+  src('src/img/**/*.{png,jpg}')
+    .pipe(cache(imagemin()))
+    .pipe(dest('build/img')) 
+  done();
+}
+
+//CSS
+function css(done){
   //Indentificar el archivo SASS
   src('src/scss/**/*.scss')
     //Compilar
@@ -21,16 +70,20 @@ function css(done){
   done(); // Callback que avisa a gulp cuando llegamos al final. done() dice que finaliza la funcion de arriba
 }
 
-function dev(done){
-  watch('src/scss/**/*', css); //Primero la direccion del archivo a escuchar y la funcion que se quiere mandar a llamar
 
+
+//Compilar con dev en npm
+function dev(done){
+  watch('src/scss/**/*', css); 
 
   done();
 }
 
 exports.css = css;
-exports.dev = dev;
-
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.versionAvif = versionAvif;
+exports.dev = parallel(imagenes,versionWebp,versionAvif,dev); //Compilan paralelamente
 
 
 
