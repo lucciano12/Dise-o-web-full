f141491eb462e24097c08290ca21870ad2f16ab2
Lucciano99 <lpradoe@hotmail.com>
2023-04-10 23:28:43 -0400

    SQL actualizado

--- Introduccion HTML/Pratica JavaScript/IntroduccionJS/js/Diferencia metodo vs funcion.js
+++
@@ -1,9 +0,0 @@
-const numero1 = 20;
-const numero2 = "20";
-
-
-console.log( parseInt(numero2) ); // parseInt() es una función (Es para transformar un string a numero)
-console.log( numero1.toString() ); // .toString() es un método ()
-
-
-/* Los metodos estan interpretao en .nombreDelMetodo */
